f6529bb4f2bc0b5fdbe3d90e808abe3d7d2aa167
vansh-000 <23165@iiitu.ac.in>
2024-08-29 15:16:10 +0530

    themes setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import db from "@/lib/supabase/db";
+import { ThemeProvider } from "@/lib/providers/next-theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,8 +18,14 @@ export default function RootLayout({
 }>) {
   return (
     console.log(db),
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    (
+      <html lang="en">
+        <body className={inter.className}>
+          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    )
   );
 }
